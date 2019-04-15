5a5b2040c9658af8f6ffc8bcd794afdc1f458c6e
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 14:32:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,9 @@ def assign_rooms(attendees)
 end
   
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  
   attendees.each do |name|
+    puts batch_badge_creator(attendees)
     puts assign_rooms(attendees)
   end
 end
