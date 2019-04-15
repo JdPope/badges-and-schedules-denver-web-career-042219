8659fa38a94389df18a2893ff5198aa327ecb5ff
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 14:10:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  
+  badges = []
+  attendees.each do |name|
+  badges.push(badge_maker(name))
+end
+  return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each do |name|
+    room_assignments.push("Hello, #{name}, You'll be assigned to room number!")
+  end
+  return room_list
+end
+  
+  
+  
+  
+  
