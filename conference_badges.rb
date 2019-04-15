d0e8d35c2ec87ba1d7bd50ec414ef7e3353adf8e
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 14:24:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index do |name, index|
-    room_assignments.push("Hello #{name}, You'll be assigned to room #{index+1}!")
+    room_assignments.push("Hello, #{name} You'll be assigned to room #{index+1}!")
   end
   return room_assignments
 end
